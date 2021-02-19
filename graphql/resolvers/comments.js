a58183e2fced1fbab767a60ee691fd729d6d1bcb
Antonella Martini <amartini@folderit.net>
2021-02-19 17:16:13 -0300

    agregado en Comment

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,7 +1,27 @@
 const { AuthenticationError, UserInputError } = require('apollo-server');
+const Post = require('../../models/Post');
 
 module.exports = {
-    Mutation:{
-        createComment: async() => {}
+    Mutation: {
+        createComment: async (_, { postId, body }, context) => {
+            const { username } = checkAuth(context);
+            if (body.trim === '') {
+                throw new UserInputError('Empty comment', {
+                    errors: {
+                        body: 'Comment body must not empty'
+                    }
+                })
+            }
+            const post = await Post.findById(postId);
+            if(post){
+                post.comments.unshift({
+                    body,
+                    username,
+                    createdAt: new Date().toISOString()
+                });
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found')
+        }
     }
 }
